fe16bc9d184861d5836c12f433eb6d15684493d8
justin890219 <67851111+justin890219@users.noreply.github.com>
2022-05-31 20:43:19 +0800

    Delete PFP-Final directory

--- PFP-Final/sort/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('sort1/', views.sort1, name='sort1'),
-    path('sort2/', views.sort2, name='sort2'),
-    path('sort3/', views.sort3, name='sort3')
-]
